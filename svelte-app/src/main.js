38e6530d75c406730f6a1cad5f263436c4aac91a
Romain Lisowski <romain.lisowski@gmail.com>
2021-04-16 04:04:55 +0200

    refactor: update siteName

--- svelte-app/src/main.js
+++ svelte-app/src/main.js
@@ -3,7 +3,8 @@ import App from './App.svelte';
 const app = new App({
 	target: document.body,
 	props: {
-		siteName: '2PLAYERS 1MINUTE'
+		siteName: 'SIDE BY SIDE',
+		baseline: 'Deux opinions en une minute'
 	}
 });
 
