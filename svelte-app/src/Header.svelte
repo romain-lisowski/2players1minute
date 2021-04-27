9f8000ab35e2ff8e7cb237b0196870894adfaebd
Romain Lisowski <romain.lisowski@gmail.com>
2021-04-27 19:37:05 +0200

    feat: active nav buttons

--- svelte-app/src/Header.svelte
+++ svelte-app/src/Header.svelte
@@ -20,6 +20,16 @@
     return categories
   })()
 
+  function getProps({ location, href, isPartiallyCurrent, isCurrent }) {
+    const isActive = href === "/" ? isCurrent : isPartiallyCurrent || isCurrent;
+
+    // The object returned here is spread on the anchor element's attributes
+    if (isActive) {
+      return { class: "active md:mx-2 px-4 py-3 hover:font-bold text-gray-800 bg-gray-100 rounded-sm hover:shadow-lg transform duration-500" };
+    } 
+    return { class: "md:mx-2 px-4 py-3 hover:font-bold text-gray-100 hover:bg-gray-100 hover:text-gray-900 rounded-sm hover:shadow-lg transform duration-500 cursor-pointer"};
+  }
+
 </script>
 
 <header class="h-full bg-gray-800 border-b-4 border-gray-500 shadow-xl">
@@ -36,7 +46,7 @@
         <div class="items-center justify-center md:flex">
           <!-- Mobile menu button -->
           <div class="md:hidden">
-            <button type="button" class="text-gray-100 hover:text-gray-200 focus:outline-none focus:text-gray-200" aria-label="Toggle menu">
+            <button type="button" class="text-gray-100 hover:text-gray-200 focus:outline-none focus:text-gray-200">
               <svg viewBox="0 0 24 24" class="w-6 h-6 fill-current">
                 <path d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"></path>
               </svg>
@@ -48,24 +58,20 @@
             <div class="flex flex-col mt-2 -mx-2 md:mt-8 md:flex-row md:block">
               {#each categories as category}
                 {#if category.icon_name === 'faHome' }
-                  <Link to={'/'}>
-                    <span class="md:mx-2 px-4 py-3 hover:font-bold text-gray-800 bg-gray-100 rounded-sm hover:shadow-lg">
-                      <Fa icon={faHome} size="lg" class="inline mr-1" />
-                      {category.name}
-                    </span>
+                  <Link to={'/'} getProps="{getProps}">
+                    <Fa icon={faHome} size="lg" class="inline mr-1" />
+                    {category.name}
                   </Link>
                 {:else }
-                  <Link to={'/' + category.id + '/' + category.name.toLowerCase()}>
-                    <span class="md:mx-2 px-4 py-3 hover:font-bold text-gray-100 hover:bg-gray-100 hover:text-gray-900 rounded-sm hover:shadow-lg transform duration-500 cursor-pointer">
-                      {#if category.icon_name === 'faGamepad' }
-                        <Fa icon={faGamepad} size="lg" class="inline mr-1" />
-                      {:else if category.icon_name === 'faFilm' }
-                        <Fa icon={faFilm} size="lg" class="inline mr-1" />
-                      {:else if category.icon_name === 'faTv' }
-                        <Fa icon={faTv} size="lg" class="inline mr-1" />
-                      {/if}
-                      {category.name}
-                    </span>
+                  <Link to={'/' + category.id + '/' + category.name.toLowerCase()} getProps="{getProps}">
+                    {#if category.icon_name === 'faGamepad' }
+                      <Fa icon={faGamepad} size="lg" class="inline mr-1" />
+                    {:else if category.icon_name === 'faFilm' }
+                      <Fa icon={faFilm} size="lg" class="inline mr-1" />
+                    {:else if category.icon_name === 'faTv' }
+                      <Fa icon={faTv} size="lg" class="inline mr-1" />
+                    {/if}
+                    {category.name}
                   </Link>
                 {/if}
               {/each}
