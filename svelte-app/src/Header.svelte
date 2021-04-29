3846aa7abd8dcbae03b55e1851dba3111a886acf
Romain Lisowski <romain.lisowski@gmail.com>
2021-04-29 20:37:10 +0200

    feat: add sitename link

--- svelte-app/src/Header.svelte
+++ svelte-app/src/Header.svelte
@@ -35,6 +35,7 @@
 <header class="h-full bg-gray-800 border-b-4 border-gray-500 shadow-xl">
     <nav class="px-10 py-6 shadow">
 
+      <Link to={'/'}>
         <div class="flex items-center justify-center">
           <img class="inline-block h-20  mr-2" src="/assets/images/gamepads.svg" alt="icon-gamepads">
           <span>
@@ -42,41 +43,42 @@
             <span class="block text-center text-xl text-gray-100 tracking-wide">{baseLine}</span>
           </span>
         </div>
+      </Link>
 
-        <div class="items-center justify-center md:flex">
-          <!-- Mobile menu button -->
-          <div class="md:hidden">
-            <button type="button" class="text-gray-100 hover:text-gray-200 focus:outline-none focus:text-gray-200">
-              <svg viewBox="0 0 24 24" class="w-6 h-6 fill-current">
-                <path d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"></path>
-              </svg>
-            </button>
-          </div>
-        
-          <!-- Mobile Menu open: "block", Menu closed: "hidden" -->
-          {#await fetchCategories then categories}
-            <div class="flex flex-col mt-2 -mx-2 md:mt-8 md:flex-row md:block">
-              {#each categories as category}
-                {#if category.icon_name === 'faHome' }
-                  <Link to={'/'} getProps="{getProps}">
-                    <Fa icon={faHome} size="lg" class="inline mr-1" />
-                    {category.name}
-                  </Link>
-                {:else }
-                  <Link to={'/' + category.id + '/' + category.name.toLowerCase()} getProps="{getProps}">
-                    {#if category.icon_name === 'faGamepad' }
-                      <Fa icon={faGamepad} size="lg" class="inline mr-1" />
-                    {:else if category.icon_name === 'faFilm' }
-                      <Fa icon={faFilm} size="lg" class="inline mr-1" />
-                    {:else if category.icon_name === 'faTv' }
-                      <Fa icon={faTv} size="lg" class="inline mr-1" />
-                    {/if}
-                    {category.name}
-                  </Link>
-                {/if}
-              {/each}
-            </div>
-          {/await}
+      <div class="items-center justify-center md:flex">
+        <!-- Mobile menu button -->
+        <div class="md:hidden">
+          <button type="button" class="text-gray-100 hover:text-gray-200 focus:outline-none focus:text-gray-200">
+            <svg viewBox="0 0 24 24" class="w-6 h-6 fill-current">
+              <path d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"></path>
+            </svg>
+          </button>
         </div>
+        
+        <!-- Mobile Menu open: "block", Menu closed: "hidden" -->
+        {#await fetchCategories then categories}
+          <div class="flex flex-col mt-2 -mx-2 md:mt-8 md:flex-row md:block">
+            {#each categories as category}
+              {#if category.icon_name === 'faHome' }
+                <Link to={'/'} getProps="{getProps}">
+                  <Fa icon={faHome} size="lg" class="inline mr-1" />
+                  {category.name}
+                </Link>
+              {:else }
+                <Link to={'/' + category.id + '/' + category.name.toLowerCase()} getProps="{getProps}">
+                  {#if category.icon_name === 'faGamepad' }
+                    <Fa icon={faGamepad} size="lg" class="inline mr-1" />
+                  {:else if category.icon_name === 'faFilm' }
+                    <Fa icon={faFilm} size="lg" class="inline mr-1" />
+                  {:else if category.icon_name === 'faTv' }
+                    <Fa icon={faTv} size="lg" class="inline mr-1" />
+                  {/if}
+                  {category.name}
+                </Link>
+              {/if}
+            {/each}
+          </div>
+        {/await}
+      </div>
     </nav>
 </header>
