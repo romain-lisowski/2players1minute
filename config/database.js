48baad2da4aca34306aa1a31ce5ed7926f46ffbd
Romain Lisowski <romain.lisowski@gmail.com>
2021-05-07 02:13:04 +0200

    feat: change db to postgresql

--- config/database.js
+++ config/database.js
@@ -4,12 +4,32 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: 'postgres',
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'strapi'),
+        username: env('DATABASE_USERNAME', 'strapi'),
+        password: env('DATABASE_PASSWORD', 'strapi'),
       },
       options: {
-        useNullAsDefault: true,
+        ssl: env.bool('DATABASE_SSL', false),
       },
     },
   },
 });
+
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'bookshelf',
+//       settings: {
+//         client: 'sqlite',
+//         filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+//       },
+//       options: {
+//         useNullAsDefault: true,
+//       },
+//     },
+//   },
+// });
