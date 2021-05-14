7fb73e4877a3e2ebe99bb8f31d5721c7aa9ea235
Romain Lisowski <romain.lisowski@gmail.com>
2021-05-14 17:08:26 +0200

    fix: trying different server for production

--- svelte-app/src/server.js
+++ svelte-app/src/server.js
@@ -1,14 +1,24 @@
-const { createServer } = require('http')
-const app = require('./dist/App.js')
+const path = require('path')
+const express = require('express')
+const app = require('./public/App.js')
 
-createServer((req, res) => {
+const server = express()
+
+server.use(express.static(path.join(__dirname, 'public')))
+
+server.get('*', function(req, res) {
   const { html } = app.render({ url: req.url })
 
   res.write(`
     <!DOCTYPE html>
+    <link rel='stylesheet' href='/global.css'>
+    <link rel='stylesheet' href='/bundle.css'>
     <div id="app">${html}</div>
-    <script src="/dist/bundle.js"></script>
+    <script src="/bundle.js"></script>
   `)
 
   res.end()
-}).listen(3000)
+})
+
+const port = 3000
+server.listen(port, () => console.log(`Listening on port ${port}`))
